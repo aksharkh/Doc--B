10fa771ad18592e782d9dc63bf8a2af763352fc3
aksharkh <aksharkh04@gmail.com>
2026-04-06 23:50:29 +0530

    added frontend connection to the backend

--- src/main/java/com/example/Doc__B/config/CorsConfig.java
+++ src/main/java/com/example/Doc__B/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:3000","https://doc-appointment-gray.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
